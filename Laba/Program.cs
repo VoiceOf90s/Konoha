cd15df518fca55b77ff78afdbcc8646aa494344a
VoiceOf90s <chtonibud86@gmail.com>
2020-01-28 12:12:12 +0300

    Lab 4

--- Laba/Program.cs
+++ Laba/Program.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Laba
+namespace Sokolov
 {
     class Program
-    {         
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double a = 0, b = 0, c = 0;
+                Console.WriteLine("Введите параметры a, b, с");
+                a = Convert.ToDouble(Console.ReadLine());
+                b = Convert.ToDouble(Console.ReadLine());
+                c = Convert.ToDouble(Console.ReadLine());
+                MyLog.log(a + "X^2+" + b + "X+" + c + "=0");
+                QuadraticEquation qe = new QuadraticEquation();
+                List<double> list = qe.resolution(a, b, c);
+                String s = "Корни: ";
+                foreach (double i in list)
+                {
+                    s += i + " ";
+                }
+                MyLog.log(s);
+            }
+            catch (GachiExсeption e)
+            {
+                MyLog.log(e.Message);
+            }
+            MyLog.write();
+            Console.ReadKey();
+        }
     }
 }
