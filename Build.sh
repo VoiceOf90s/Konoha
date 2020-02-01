70897e1ec38e3c653c65706ee74f2b8c5b9fb348
VoiceOf90s <chtonibud86@gmail.com>
2020-02-01 10:50:15 +0300

    Lab 9

---
+++ Build.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/VoiceOf90s/Konoha-2.0.git Laba/core
+git symbolic-ref --short -q HEAD>Laba/bin/Debug/version.txt
--- Laba/Sokolov/MyLog.cs
+++ Laba/Sokolov/MyLog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,9 +23,24 @@ namespace Sokolov
         }
         override public void _write()
         {
+            string path = @"..\..\..\version.txt";
+            Directory.CreateDirectory("Log");
+            using(StreamReader sr = new StreamReader(path))
+            {
+                MyLog.log("Версия: " + sr.ReadToEnd().Trim('\n'));
+            }
+            string date = DateTime.Now.ToString("dd.MM.yyyy_HH_mm_ss");
             for (int i = 0; i < _log.Count; i++)
             {
                 Console.WriteLine(_log[i]);
+            }
+            string datename = @"Log" +Path.DirectorySeparatorChar + date + ".log";
+            using (StreamWriter we = new StreamWriter(datename, false, System.Text.Encoding.Default))
+            {
+                for(int i = 0; i < _log.Count; i++)
+                {
+                    we.WriteLine(_log[i]);
+                }
             }
         }
     }
